8eb19e6a1eb01a69d81e1c04b2d4023391bd8d2d
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-09 23:35:22 +0600

    Homepage Added and Basic Setup Completed

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>
+      }
+    ]
   },
 ]);
 
